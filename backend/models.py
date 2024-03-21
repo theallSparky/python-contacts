c8b99a5d6f81d783d41b4d0e017253db4df7b0ad
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-20 21:44:27 -0500

    Did some more work on the Contact class

--- backend/models.py
+++ backend/models.py
@@ -3,3 +3,11 @@ from config import db
 class Contact(db.Model):
     id = db.Coloumn(db.Integer, primary_key=True)
     first_name = db.Cloumn(db.String(80), unique=False, nullable=False)
+    last_name = db.Cloumn(db.String(80), unique=False, nullable=False)
+    email = db.Cloumn(db.String(120), unique=True, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName":self.first_name,
+        }
