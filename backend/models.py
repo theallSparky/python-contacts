0eeba99a1bbc1c337d5b04e5df582b5b4881bf98
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-21 20:24:16 -0500

    Fixed bugs preventing db connection. Now connected

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,10 @@
 from config import db
 
 class Contact(db.Model):
-    id = db.Coloumn(db.Integer, primary_key=True)
-    first_name = db.Cloumn(db.String(80), unique=False, nullable=False)
-    last_name = db.Cloumn(db.String(80), unique=False, nullable=False)
-    email = db.Cloumn(db.String(120), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
     def to_json(self):
         return {
