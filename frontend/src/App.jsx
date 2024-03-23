a884ff2a08dad5cee89d81ce04a184bd41fff454
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-22 23:30:50 -0500

    Fixed title misspelling, added fetchcontacts logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data.contacts);
+  };
   return <></>;
 }
 
