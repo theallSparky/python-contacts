40ee2502b91fdbd1248e70f3072bdc78372e2d89
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-22 19:21:48 -0500

    Made "/create_contact" route. Started on "update_contact/<int:user_id">

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,32 @@ def get_contacts():
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
 
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    if not first_name or not last_name or not email:
+        return jsonify({"message": "You must include a first name, a lsat name, and an email!"}),
+        400,
+
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "User created!"}), 201
+
+@app.route("update_contact/<int:user_id>")
+def update_contact(user_id):
+    contact =   Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"})
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
