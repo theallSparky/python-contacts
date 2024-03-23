7b2b29aa44c423a5d58ecead7d85f5375bb88916
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-22 23:39:32 -0500

    Made contactlist component, added table head

---
+++ frontend/src/ContactList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ContactList = ({ contacts }) => {
+  return (
+    <div>
+      <h2>Contacts</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  );
+};
